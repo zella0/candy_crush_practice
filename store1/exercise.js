4e9e5e3da8c62c1488178fdee4dbb40d5645a941
Kim Schlesinger <coloradokim@users.noreply.github.com>
2016-11-23 09:38:23 -0700

    Update exercise.js
    Added another question.

--- store1/exercise.js
+++ store1/exercise.js
@@ -8,9 +8,6 @@ var store1 = require('./store1-data.js');
 // How would you access how many 'Mint Wafers' were sold on January 6th?
 
 
-// Produce an array of the date keys in store1's data.
-
-
 // *********** LOOPING OVER DATA *********** \\
 
 
@@ -20,10 +17,14 @@ var store1 = require('./store1-data.js');
 // Create a loop to count the total number of candies sold on Jan 10 at store1. Where do you have to initialize the counter variable? Why?
 
 
+// Use `Object.keys()` to get an array of the dates that candies were sold at store1.
+
+
 // Iterate over the generated array of dates. Use each date to console.log the specific sale data for the day from store1.
 
 
 // Use a loop to calculate the total number of candies sold at store1.
+
 
 // In the previous exercise, where did you have to initialize the counter variable? Why?
 
