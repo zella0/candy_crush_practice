c299e010ab33b834777183d25cef617666e41f82
chadwithuhc <chadwithuhc@gmail.com>
2016-12-20 14:19:08 -0700

    Store2 Tests

--- store2/exercise.js
+++ store2/exercise.js
@@ -2,28 +2,64 @@ var store2 = require('./store2-data.js');
 
 /////////// ACCESSING DATA ///////////
 
-// Set the sale dates for 'Banana Bunches' from store2 to a variable.
+// Set the sale dates for 'Banana Bunches' from store2 to a variable, then return that variable
+function accessesingData1() {
 
+}
 
-// What built-in array property would you use to determine how many sale dates there for store2's 'Banana Bunches'?
+// Using a built-in array property return the total sale dates there are for store2's 'Banana Bunches'
+function accessesingData2() {
 
+}
 
 /////////// UPDATING DATA ///////////
 
-// Set a variable to equal the prices of the 'Mint Wafers' in store2. Then reassign it to a new value. When you update your new variable, does the price in the store2 data change? Why or why not?
+// Set a variable to equal the prices of the 'Mint Wafers' in store2. Then reassign it to a new value. When you update your new variable, does the price in the store2 data change? Return your answer as a boolean and write a comment on why or why not.
+function updatingData1() {
 
+}
 
-// Set a variable equal to the Peanut Butter Buttered Peanuts sale dates in store2. Use pop to remove one of the sale dates for store2's Caramel Twists.
+// Set a variable equal to the 'Caramel Twists' sale dates in store2. Use pop to remove one of the sale dates for store2's 'Caramel Twists'. Does your variables value match up with what is in store2? Return your answer as a boolean and write a comment on why or why not.
+function updatingData2() {
+
+}
 
 /////////// LOOPING OVER DATA ///////////
 
-// Iterate over the store2 sale dates for Caramel Twists. Use this to create an object containing dates as the key and the value as the quantity sold on that day.
+// Iterate over the store2 sale dates for 'Caramel Twists'. Use this to create an object containing dates as the key and the value as the quantity sold on that day.
+// Example:
+// {
+//   '2015-01-06': 4,
+//   '2015-01-07': 3,
+// }
+function loopingData1() {
 
+}
 
-// Iterate over store2's sale dates data to find which day had the most total number of sales. How did you calculate this?
+// Iterate over store2's sale dates data to find which day had the most total number of sales. Return the date with the most sales.
+function loopingData2() {
 
+}
 
 /////////// CHALLENGE ///////////
 
-// Create an object containing the amounts that store2 made by selling each type of candy.
+// Create and return an object containing the amounts that store2 made by selling each type of candy. Round each number to 2 decimal points.
+// Example:
+// {
+//   'Caramel Twists': 14.15,
+//   'Peppermint Poppers': 20.51
+// }
+function challenge1() {
 
+}
+
+
+module.exports = {
+  accessesingData1,
+  accessesingData2,
+  updatingData1,
+  updatingData2,
+  loopingData1,
+  loopingData2,
+  challenge1
+}
---
+++ store2/exercise.spec.js
@@ -0,0 +1,57 @@
+var assert = require('assert')
+var exercise = require('./exercise')
+
+describe('Store2 Exercises', function() {
+  describe('#accessesingData1()', function () {
+    it('returns array of dates', function () {
+      assert.deepEqual(exercise.accessesingData1(), ['2015-01-10', '2015-01-10'])
+    })
+  })
+  describe('#accessesingData2()', function () {
+    it('returns 2', function () {
+      assert.equal(exercise.accessesingData2(), 2)
+    })
+  })
+  describe('#updatingData1()', function () {
+    it('returns boolean', function () {
+      assert.deepEqual(exercise.updatingData1(), false)
+    })
+  })
+  describe('#updatingData2()', function () {
+    it('returns boolean', function () {
+      assert.deepEqual(exercise.updatingData2(), true)
+    })
+  })
+  describe('#loopingData1()', function () {
+    it('returns object with dates as key and quantity as value', function () {
+      var result = {
+        '2015-01-06': 2,
+        '2015-01-07': 1,
+        '2015-01-08': 2,
+        '2015-01-09': 2,
+        '2015-01-10': 2
+      }
+      assert.deepEqual(exercise.loopingData1(), result)
+    })
+  })
+  describe('#loopingData2()', function () {
+    it('returns date string', function () {
+      assert.equal(exercise.loopingData2(), '2015-01-10')
+    })
+  })
+
+  describe('#challenge1()', function () {
+    it('returns an object of candies and total revenue rounded to two numbers', function () {
+      var result = {
+        'Dark Chocolate Crunchies': '17.16',
+        'Mint Wafers': '4.36',
+        'Peppermint Poppers': '16.66',
+        'Peanut Butter Buttered Peanuts': '3.58',
+        'Berry Bites': '47.34',
+        'Caramel Twists': '4.50',
+        'Banana Bunches': '9.06'
+      }
+      assert.deepEqual(exercise.challenge1(), result)
+    })
+  })
+})
