41381d63d5b9b55ef4d12275c0d7ec196b7217c7
chadwithuhc <chadwithuhc@gmail.com>
2016-12-20 12:44:38 -0700

    Testing for Store1

--- readme.md
+++ readme.md
@@ -2,8 +2,13 @@
 
 A local chain of candy stores has hired you to draw conclusions from their sales data. This is made difficult by the fact that each store keeps track of their own inventory sales in a slightly different manner. Use the data sets contained in each folder to complete the exercises.
 
+### Setup
+Clone the repo down to your local environment. Run `npm install` to get testing packages.
+
 ### Recommended execution of these exercises
-Write your code as functions that result in a ```console.log()``` and then run ```node (filename)``` on the command line.
+Write your code as functions that return the result and then run ```node test ./[foldername]``` on the command line.
+
+Example: `node test ./store1`
 
 ### Tips
 * Be sure to review dot notation vs. bracket notation and when you need to use one over the other.
---
+++ store1/exercise.spec.js
@@ -0,0 +1,47 @@
+var assert = require('assert')
+var exercise = require('./exercise')
+
+describe('Store1 Exercises', function() {
+  describe('#accessesingData1()', function () {
+    it('returns \'4.63\'', function () {
+      assert.equal(exercise.accessesingData1(), '4.63')
+    })
+  })
+  describe('#accessesingData2()', function () {
+    it('returns 3', function () {
+      assert.equal(exercise.accessesingData2(), 3)
+    })
+  })
+  describe('#loopingData1()', function () {
+    it('returns array of candy names', function () {
+      var result = ['Banana Bunches', 'Peppermint Poppers', 'Mint Wafers', 'Dark Chocolate Crunchies', 'Caramel Twists']
+      assert.deepEqual(exercise.loopingData1(), result)
+    })
+  })
+  describe('#loopingData2()', function () {
+    it('returns 22', function () {
+      assert.equal(exercise.loopingData2(), 22)
+    })
+  })
+  describe('#loopingData3()', function () {
+    it('returns array of date strings', function () {
+      var result = ['2015-01-06', '2015-01-07', '2015-01-08', '2015-01-09', '2015-01-10']
+      assert.deepEqual(exercise.loopingData3(), result)
+    })
+  })
+  describe('#loopingData4()', function () {
+    it('returns total number of items sold', function () {
+      assert.equal(exercise.loopingData4(), 68)
+    })
+  })
+  describe('#loopingData5()', function () {
+    it('returns a string of your answer', function () {
+      assert.notEqual(exercise.loopingData5(), 'Answer')
+    })
+  })
+  describe('#challenge1()', function () {
+    it('returns a number of total revenue, rounded to two numbers', function () {
+      assert.deepEqual(exercise.challenge1(), 75.90)
+    })
+  })
+})
