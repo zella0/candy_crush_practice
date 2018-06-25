fa1e61fb152b60646978e4febe5e426de8125d20
Nhan Phan <nani@Nhans-MacBook-Pro.local>
2018-06-25 10:17:48 -0700

    only for store3/store4

--- store3/index.js
+++ store3/index.js
@@ -4,24 +4,28 @@ var store3 = require('./store3-data.js');
 
 // Return Berry Bites data for store3 on January 7.
 function accessesingData1() {
-
+  return store3[1]["inventory sold"]["Berry Bites"];
 }
-
+// accessesingData1(store3);
 // Return how many Mint Wafers were sold on January 9th
-function accessesingData2() {
-
+function accessesingData2(){
+  return store3[3]["inventory sold"]["Mint Wafers"].quantity;
 }
 
 // Return total number of dates included in the dataset.
-function accessesingData3() {
-
+function accessesingData3(){
+  return store3.length;
 }
 
 //()()()()() LOOPING OVER DATA ()()()()()\\
 
 // Create a loop to iterate over the sale data from store3. Use this loop to return an array of dates contained in the sale data.
 function loopingData1() {
-
+  var arr = [];
+  for(var i = 0; i < store3.length; i++){
+    arr.push(store3[i].date)
+  }
+  return arr;
 }
 
 // Use `Object.keys()` to loop over the inventory sold for January 10th in store3. While iterating over the data, create an object containing each candy's name and price. Return the result.
@@ -32,20 +36,40 @@ function loopingData1() {
 //   ...
 // }
 function loopingData2() {
-
+	var obj = store3[4]["inventory sold"];
+  var objResult = {
+  }
+  for(let key in obj){
+    objResult[key] = obj[key].cost;
+  }
+  return objResult
 }
 
 // Create a loop to iterate over the whole store3 variable to find out how many Peanut Butter Buttered Peanuts were sold over all dates. Return the result.
 function loopingData3() {
-
+  var resultAcc = 0;
+  for(let key in store3){
+    resultAcc += store3[key]["inventory sold"]["Peanut Butter Buttered Peanuts"].quantity;
+  }
+  return resultAcc;
 }
 
 //()()()()() CHALLENGE ()()()()()\\
 
 // Determine how much money store3 made on January 9th and return the result fixed to two decimal points
-function challenge1() {
-
+function challenge1(){
+  var result = 0;
+  var objInventory = store3[3]["inventory sold"];
+  for(let key in objInventory){
+    // console.log(objInventory[key].quantity)
+    // console.log(objInventory[key].cost)
+   result += Number((objInventory[key].quantity * objInventory[key].cost));
+  }
+  result = result.toFixed(2);
+  result = Number(result)
+  return result;
 }
+
 
 module.exports = {
   accessesingData1,
--- store4/index.js
+++ store4/index.js
@@ -1,41 +1,77 @@
 var store4 = require('./store4-data.js');
 
 // ^^^^^^^^^^^^ ACCESSING DATA ^^^^^^^^^^^^
-
 // Return the cost of 'Dark Chocolate Crunchies' from store4
 function accessesingData1() {
-
+  return store4["Dark Chocolate Crunchies"].cost;
 }
 
 // Return the total number of 'Berry Bites' sold by store4
 function accessesingData2() {
-
+  return store4["Berry Bites"]["sold on"].length;
 }
 
 // ^^^^^^^^^^^^ LOOPING OVER DATA ^^^^^^^^^^^^
 // Create a loop to return an array of the candy names that cost more than $2.00
 function loopingData1() {
-
+  let arrResult = [];
+  for(let key in store4){
+    if(store4[key].cost > 2){
+      arrResult.push(key)
+    }
+  }
+  return arrResult;
 }
 
 // Create a loop to return an object that has the candy name as the key and the cost as the value
 function loopingData2() {
+  var obj = {
+  }
 
+  for(let key in store4){
+      obj[key] = store4[key].cost;
+  }
+  return obj;
 }
+
 
 // ^^^^^^^^^^^^ CHALLENGE ^^^^^^^^^^^^
 // Calculate how much money store4 made selling Peppermint Poppers. What about Caramel Twists? Each date represents 1 item sold. Round revenue to two decimal points. Return your answer as an array: `[total1, total2]`
 function challenge1() {
-
+  let arrResult = []
+  for(let key in store4){
+    if(key == 'Peppermint Poppers'){
+      let total = store4[key]["sold on"].length * store4[key].cost;
+      arrResult.push(total)
+    }else if(key == 'Caramel Twists'){
+      let total = store4[key]["sold on"].length * store4[key].cost;
+      total = Number(total.toFixed(2));
+      arrResult.push(total);
+    }
+  }
+  return arrResult;
 }
 
 // Calculate and return how much money store4 made on January 9th. Round to two decimal points.
 function challenge2() {
-
+  let result = 0;
+  for(let key in store4){
+    let candySold = store4[key]['sold on'].length;
+    for(let i = 0; i < candySold; i++){
+      if(store4[key]['sold on'][i] === '2015-01-09'){
+        result += store4[key].cost;
+      }
+    }
+  }
+  return result;
 }
 
 // Calculate and return how much store4 made from selling Dark Chocolate Crunchies across all the days. Round to two decimal points.
 function challenge3() {
+  // let result = 0;
+  // for(let i = 0; i < store4["Dark Chocolate Crunchies"]["sold on"].length; i++){
+  //   console.log(store4["Dark Chocolate Crunchies"]["sold on"][i])
+  return store4["Dark Chocolate Crunchies"]["sold on"].length * 4.34;
 
 }
 
